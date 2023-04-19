0269fc3018d47f5a58d5947768999e5ba244c603
Павел Деменчук <demenchuk.po@dclouds.ru>
2023-04-19 18:09:26 +0700

    adding html reporting

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
+
     testMatch: [
         '**/spec/**/*.test.js'
+    ],
+    reporters: [
+        "default",
+        ["jest-html-reporters", {
+            publicPath: "./html-report",
+            filename: "report.html",
+            pageTitle: "Test Report",
+            expand: true
+        }]
     ],
 };
